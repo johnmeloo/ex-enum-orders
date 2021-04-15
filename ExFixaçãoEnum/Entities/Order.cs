e7a2d4fde96161d16c61f0da178584f099b235b1
Johnney Melo <johnneymelo@gmail.com>
2021-04-15 00:11:10 -0300

    Classes Criadas

---
+++ ExFixaçãoEnum/Entities/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ExFixaçãoEnum.Entities;
+using ExFixaçãoEnum.Entities.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExFixaçãoEnum.Entities
+{
+    class Order
+    {
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime date, OrderStatus status)
+        {
+            Date = date;
+            Status = status;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total(double price, int quantity)
+        {
+            double sum = 0.0;
+            foreach (OrderItem order in Items)
+            {
+                sum += order.SubTotal();
+            }
+            return sum;
+        }
+    }
+}
---
+++ ExFixaçãoEnum/Entities/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using ExFixaçãoEnum.Entities;
+namespace ExFixaçãoEnum.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
