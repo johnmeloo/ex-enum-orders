4fe9c7d4880929b9ee3de1eebbc7d5566a7d5cea
Johnney Melo <johnneymelo@gmail.com>
2021-04-14 23:01:38 -0300

    Inciando Projeto

---
+++ ExFixaçãoEnum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExFixaçãoEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
